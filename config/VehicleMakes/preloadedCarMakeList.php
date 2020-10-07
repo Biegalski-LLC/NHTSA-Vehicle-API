1cc9976f4be478d348b48ec686a48364016b9e83
Franco Petitfour <franco.petitfour001@gmail.com>
2020-10-07 11:41:14 -0400

    Added Vehicle Makes by Vehicle Type Name

---
+++ config/VehicleMakes/preloadedCarMakeList.php
@@ -0,0 +1,260 @@
+<?php
+
+return [
+    [
+        'make_id' => 475,
+        'make_name' => 'Acura'
+    ],
+    [
+        'make_id' => 493,
+        'make_name' => 'Alfa Romeo'
+    ],
+    [
+        'make_id' => 606,
+        'make_name' => 'AM General'
+    ],
+    [
+        'make_id' => 440,
+        'make_name' => 'Aston Martin'
+    ],
+    [
+        'make_id' => 582,
+        'make_name' => 'Audi'
+    ],
+    [
+        'make_id' => 583,
+        'make_name' => 'Bentley'
+    ],
+    [
+        'make_id' => 452,
+        'make_name' => 'BMW'
+    ],
+    [
+        'make_id' => 454,
+        'make_name' => 'Bugatti'
+    ],
+    [
+        'make_id' => 468,
+        'make_name' => 'Buick'
+    ],
+    [
+        'make_id' => 469,
+        'make_name' => 'Cadillac'
+    ],
+    [
+        'make_id' => 467,
+        'make_name' => 'Chevrolet'
+    ],
+    [
+        'make_id' => 477,
+        'make_name' => 'Chrysler'
+    ],
+    [
+        'make_id' => 1077,
+        'make_name' => 'Daewoo'
+    ],
+    [
+        'make_id' => 476,
+        'make_name' => 'Dodge'
+    ],
+    [
+        'make_id' => 2408,
+        'make_name' => 'Eagle'
+    ],
+    [
+        'make_id' => 603,
+        'make_name' => 'Ferrari'
+    ],
+    [
+        'make_id' => 492,
+        'make_name' => 'FIAT'
+    ],
+    [
+        'make_id' => 1033,
+        'make_name' => 'Fisker Automotive'
+    ],
+    [
+        'make_id' => 460,
+        'make_name' => 'Ford'
+    ],
+    [
+        'make_id' => 5083,
+        'make_name' => 'Genesis'
+    ],
+    [
+        'make_id' => 1146,
+        'make_name' => 'Geo'
+    ],
+    [
+        'make_id' => 472,
+        'make_name' => 'GMC'
+    ],
+    [
+        'make_id' => 474,
+        'make_name' => 'Honda'
+    ],
+    [
+        'make_id' => 951,
+        'make_name' => 'Hummer'
+    ],
+    [
+        'make_id' => 498,
+        'make_name' => 'Hyundai'
+    ],
+    [
+        'make_id' => 480,
+        'make_name' => 'Infiniti'
+    ],
+    [
+        'make_id' => 542,
+        'make_name' => 'Isuzu'
+    ],
+    [
+        'make_id' => 442,
+        'make_name' => 'Jaguar'
+    ],
+    [
+        'make_id' => 483,
+        'make_name' => 'Jeep'
+    ],
+    [
+        'make_id' => 499,
+        'make_name' => 'Kia'
+    ],
+    [
+        'make_id' => 502,
+        'make_name' => 'Lamborghini'
+    ],
+    [
+        'make_id' => 444,
+        'make_name' => 'Land Rover'
+    ],
+    [
+        'make_id' => 515,
+        'make_name' => 'Lexus'
+    ],
+    [
+        'make_id' => 464,
+        'make_name' => 'Lincoln'
+    ],
+    [
+        'make_id' => 466,
+        'make_name' => 'Lotus'
+    ],
+    [
+        'make_id' => 443,
+        'make_name' => 'Maserati'
+    ],
+    [
+        'make_id' => 533,
+        'make_name' => 'Maybach'
+    ],
+    [
+        'make_id' => 473,
+        'make_name' => 'Mazda'
+    ],
+    [
+        'make_id' => 2236,
+        'make_name' => 'McLaren'
+    ],
+    [
+        'make_id' => 449,
+        'make_name' => 'Mercedes-Benz'
+    ],
+    [
+        'make_id' => 465,
+        'make_name' => 'Mercury'
+    ],
+    [
+        'make_id' => 456,
+        'make_name' => 'Mini'
+    ],
+    [
+        'make_id' => 481,
+        'make_name' => 'Mitsubishi'
+    ],
+    [
+        'make_id' => 478,
+        'make_name' => 'Nissan'
+    ],
+    [
+        'make_id' => 4162,
+        'make_name' => 'Oldsmobile'
+    ],
+    [
+        'make_id' => 4220,
+        'make_name' => 'Panoz'
+    ],
+    [
+        'make_id' => 2409,
+        'make_name' => 'Plymouth'
+    ],
+    [
+        'make_id' => 10224,
+        'make_name' => 'Polestar'
+    ],
+    [
+        'make_id' => 536,
+        'make_name' => 'Pontiac'
+    ],
+    [
+        'make_id' => 584,
+        'make_name' => 'Porsche'
+    ],
+    [
+        'make_id' => 496,
+        'make_name' => 'Ram'
+    ],
+    [
+        'make_id' => 445,
+        'make_name' => 'Rolls-Royce'
+    ],
+    [
+        'make_id' => 572,
+        'make_name' => 'Saab'
+    ],
+    [
+        'make_id' => 1056,
+        'make_name' => 'Saturn'
+    ],
+    [
+        'make_id' => 504,
+        'make_name' => 'smart'
+    ],
+    [
+        'make_id' => 3766,
+        'make_name' => 'Spyker'
+    ],
+    [
+        'make_id' => 523,
+        'make_name' => 'Subaru'
+    ],
+    [
+        'make_id' => 509,
+        'make_name' => 'Suzuki'
+    ],
+    [
+        'make_id' => 441,
+        'make_name' => 'Tesla'
+    ],
+    [
+        'make_id' => 448,
+        'make_name' => 'Toyota'
+    ],
+    [
+        'make_id' => 482,
+        'make_name' => 'Volkswagen'
+    ],
+    [
+        'make_id' => 485,
+        'make_name' => 'Volvo'
+    ],
+    [
+        'make_id' => 487,
+        'make_name' => 'Volvo Truck'
+    ],
+    [
+        'make_id' => 525,
+        'make_name' => 'Workhorse'
+    ]
+];
